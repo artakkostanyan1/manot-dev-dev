72de7676833e624cf614ecb50d3de53ad5549c6e
HakonyAnna <anna.hakobyan.im.itc@gmail.com>
2021-08-25 23:53:06 +0400

    - Added sing up button - Added forgot password link

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+import { Button, Link } from '@material-ui/core';
 import './LogIn.scss';
 
 function LogIn(props) {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
     const history = useHistory();
+    const preventDefault = (event) => {
+        event.preventDefault();
+        history.push('/registration')
+    };
 
     return (
         <>
             <Header />
+            <div className='button-container'>
+                <Button onClick={() => history.push('/registration')}>
+                    Sign Up
+                </Button>
+            </div>
             <div className='form_wrapper'>
                 <form className='form' onSubmit={handleSubmit((data) => console.log(data))}>
                     <div className='heading'>Login</div>
@@ -27,6 +37,13 @@ function LogIn(props) {
                         {...register("password", { required: 'Please enter password' })} />
                     {errors.password && <div className='error_message'>{errors.password.message}</div>}
 
+                    <Link
+                        href="#"
+                        onClick={preventDefault}
+                        className='forggot-pasword'
+                    >
+                        Forgot password?
+                    </Link>
                     <button
                         onClick={() => history.push('/')}
                         className='save_button'
