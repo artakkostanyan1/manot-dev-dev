b74ab51fe2e511e65f3717971e6551b67aa42ebc
Elya <elya_hakobyan_2019@mail.ru>
2021-08-27 01:17:32 +0400

    fixed sign up page

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import { useForm } from "react-hook-form";
-import { useHistory } from 'react-router';
+// import { useHistory } from 'react-router';
+
+import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@material-ui/icons/VisibilityOffOutlined';
+
 import './Registration.scss';
 
 function ProfileAndPassword(props) {
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const history = useHistory();
-    
+    const [passwordType, setPasswordType] = useState('text');
+    const [repeatPasswordType, setRepeatPasswordType] = useState('text');
+    // const history = useHistory();
+
+    function handleClick1() {
+        (passwordType === 'text') ? setPasswordType('password') : setPasswordType('text');
+    }
+
+    function handleClick2() {
+        (repeatPasswordType === 'text') ? setRepeatPasswordType('password') : setRepeatPasswordType('text');
+    }
     const onSubmit = data => console.log(data);
 
     return (
@@ -15,37 +28,61 @@ function ProfileAndPassword(props) {
             <Header />
             <div className='form_wrapper'>
                 <form className='form' onSubmit={handleSubmit((data) => console.log(data))}>
-                    <div className='heading'>Profile and password</div>
+                    <div className='heading'>Sign up</div>
 
-                    <div className='full_name_div'>Full name</div>
-                    <input className="full_name_input"
-                        {...register("fullname", { required: 'Please enter your full name' })} />
-                    {errors.fullname && <div className='error_message'>{errors.fullname.message}</div>}
+                    {/* <div className='name_div'>Name</div> */}
+                    <input className="name_input"
+                        {...register("name", { required: 'Please enter your name' })}
+                        placeholder='Name' />
+                    {errors.name && <div className='error_message'>{errors.name.message}</div>}
 
-                    <div className='email_div'>Email</div>
+                    {/* <div className='surname_div'>Surname</div> */}
+                    <input className="surname_input"
+                        {...register("surname", { required: 'Please enter your full name' })}
+                        placeholder='Surname' />
+                    {errors.surname && <div className='error_message'>{errors.surname.message}</div>}
+
+                    {/* <div className='email_div'>Email</div> */}
                     <input className="email_input"
                         type='email'
-                        {...register("email", { required: 'Please enter your email' })} />
+                        {...register("email", { required: 'Please enter your email' })}
+                        placeholder='Email' />
                     {errors.email && <div className='error_message'>{errors.email.message}</div>}
 
-                    <div className='new_password_div'>New password</div>
-                    <input className="new_password_input"
-                        {...register("newpassword", { required: 'Please enter password' })} />
+                    <br />
+                    <div className='pass_wrapper'>
+                        <input type={passwordType} className='new_password_input'
+                            {...register("newpassword", { required: 'Please enter password' })}
+                            placeholder='Password'
+                        />
+                        <button className='pass_button' onClick={handleClick1}>
+                            {(passwordType === 'text') ? <VisibilityOutlinedIcon style={{ fontSize: '22' }} />
+                                : <VisibilityOffOutlinedIcon style={{ fontSize: '22' }} />
+                            }
+                        </button>
+                    </div>
                     {errors.newpassword && <div className='error_message'>{errors.newpassword.message}</div>}
 
-                    <div className='repeat_password_div'>Repeat new password</div>
-                    <input className="repeat_password_input"
-                        {...register("repeatpassword", { required: 'Please repeat password' })} />
-                    {errors.repeatpassword && <div className='error_message'>{errors.repeatpassword.message}</div>}
-                    <br />
+                    <div className='pass_wrapper'>
+                        <input type={repeatPasswordType} className='new_password_input'
+                            {...register("newpassword", { required: 'Please enter password' })}
+                            placeholder='Repeat password'
+                        />
+                        <button className='pass_button' onClick={handleClick2}>
+                            {(repeatPasswordType === 'text') ? <VisibilityOutlinedIcon style={{ fontSize: '22' }} />
+                                : <VisibilityOffOutlinedIcon style={{ fontSize: '22' }} />
+                            }
+                        </button>
+                    </div>
+                    {errors.newpassword && <div className='error_message'>{errors.newpassword.message}</div>}
 
                     <button
-                        onClick={() => history.push('/verification')}
+                        // onClick={() => history.push('/verification')}
                         className='submit_button'
                         type='submit'
                     >
                         <div className='submit_text'>
-                            Save changes
+                            Sign up
                         </div>
                     </button>
 
