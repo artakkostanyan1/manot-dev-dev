affb32a8f87fe0fe0446e7ab78d4c7372ba9f084
ElyaHakobyan <elya_hakobyan_2019@mail.ru>
2021-09-20 02:24:54 +0400

    finalized upgrade page

--- src/pages/Upgrade/Upgrade.jsx
+++ src/pages/Upgrade/Upgrade.jsx
@@ -1,13 +1,22 @@
+import UserHeader from '../../components/UserHeader/UserHeader';
 import UpgradeCard from '../../components/UpgrageCard/UpgradeCard';
 import './Upgrade.scss';
 
 function Upgrade(props) {
     return (
-        <div className='upgrade_container'>
-            <UpgradeCard header='Free' price='0' />
+        <>
+            <UserHeader />
+            <div className='most_popular_container'>
+                <div className='most_popular_div'>
+                    Most popular
+                </div>
+            </div>
+            <div className='upgrade_container'>
+                <UpgradeCard header='Free' price='0' />
                 <UpgradeCard header='Standart' price='108' hasShadow={true} />
-            <UpgradeCard header='Enterprise' price='200' />
-        </div>
+                <UpgradeCard header='Enterprise' price='200' />
+            </div>
+        </>
     )
 }
 
