cdf9c74e4e4ad5c14acfc61563d17c0f6c837fd0
ElyaHakobyan <elya_hakobyan_2019@mail.ru>
2021-09-24 23:36:35 +0400

    FD-31/added information of upgrade page, made page structure better

---
+++ src/utils/upgrade.jsx
@@ -0,0 +1,25 @@
+const upgradeData = {
+    Free: {
+        subHeader: 'For anyone to get started',
+        price: 0,
+        body: [
+            'up to 500 images', 'single user', 'up to 1024x1024 resolution size', 'detection on single image', 'manual rotated and bounding box annotation', '10% manual correction'
+        ]
+    },
+    Business: {
+        subHeader: 'For solo preneurs, bootstrappers, early stage startups',
+        price: 29,
+        body: [
+            'up to 2000 images', 'single user', 'up to 1024x1024 resolution size', 'detection on single image', 'full data automated annotation', 'video annotation(coming soon)', 'manual rotated and bounding box annotation', '10-15% manual correction'
+        ]
+    },
+    Enterprise: {
+        subHeader: 'Per image type and image number',
+        price: 100,
+        body: [
+            'unlimited images', 'multiple users', 'up to 4096x4096 resolution size', 'detection on single image', 'full data automated annotation', 'video annotation(coming soon)', 'manual rotated and bounding box annotation', '10-15% manual correction'
+        ]
+    }
+}
+
+export default upgradeData;
