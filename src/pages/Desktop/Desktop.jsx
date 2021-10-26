6adeac276f67ec5ef3d193ec9e3d4b5f7652a2a7
ElyaHakobyan <elya_hakobyan_2019@mail.ru>
2021-10-26 16:01:02 +0400

    initial changes in structure, screenshot is in jira

--- src/pages/Desktop/Desktop.jsx
+++ src/pages/Desktop/Desktop.jsx
@@ -6,11 +6,23 @@ import './Desktop.scss';
 function Desktop() {
     return (
         <div className='comp-desktop'>
-            <UserHeader className='user_header_desktop'/>
+            <UserHeader className='user_header_desktop' />
             <div className='main-content-container'>
-                <LeftBar className='left_bar'/>
+                <LeftBar className='left_bar' />
                 <div className='main-photo'>
-                    Main photo
+                    <img
+                    className='selected_image'
+                        alt='girl'
+                        src="horses-1.png"
+                        className='label-photo'
+
+                        style={{
+                            height: '100%',
+                            marginLeft: '7px',
+                            marginRight: '7px'
+
+                        }}
+                    />
                 </div>
                 <RightBar />
             </div>
