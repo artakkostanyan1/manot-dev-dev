dee1c942dd783a34a8702636d91ae4b16b7bf784
HakonyAnna <anna.hakobyan.im.itc@gmail.com>
2021-12-02 01:52:44 +0400

    Corrected issue with routhings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-// import Loader from './components/Loader/Loader'
 import ImportData from './pages/ImportData/ImportData';
 import Main from './pages/Main/Main';
 import Registration from './pages/Registration/Registration';
@@ -32,171 +31,35 @@ const NoMatchPage = () => {
   return <h3>404 - Not found</h3>;
 };
 
-// const publicRoutes = [
-//   {
-//     path: paths.ResetPassword,
-//     exact: true,
-//     private: false,
-//     component: ResetPassword,
-//   },
-//   {
-//     path: paths.OldVerification,
-//     exact: true,
-//     private: false,
-//     component: Verification,
-//   },
-//   {
-//     path: paths.Login,
-//     exact: true,
-//     private: false,
-//     component: LogIn,
-//   },
-//   {
-//     path: paths.Verify,
-//     exact: true,
-//     private: false,
-//     component: VerifyEmail,
-//   },
-//   {
-//     path: paths.Email,
-//     exact: true,
-//     private: false,
-//     component: Email,
-//   },
-//   {
-//     path: paths.SignUp,
-//     exact: true,
-//     private: false,
-//     component: Registration,
-//   },
-//   {
-//     path: '/terms',
-//     exact: true,
-//     private: false,
-//     component: Terms,
-//   },
-//   {
-//     path: '/policy',
-//     exact: true,
-//     private: false,
-//     component: Policy,
-//   },
-//   {
-//     path: '/contact-us',
-//     exact: true,
-//     private: false,
-//     component: ContactUs,
-//   },
-//   {
-//     path: paths.NewRegistration,
-//     exact: true,
-//     private: false,
-//     component: NewRegistration,
-//   },
-//   {
-//     path: paths.NewLogin,
-//     exact: true,
-//     private: false,
-//     component: NewLogin,
-//   },
-//   {
-//     path: paths.NewEmail,
-//     exact: true,
-//     private: false,
-//     component: NewEmail
-//   },
-//   {
-//     path: paths.Main,
-//     exact: true,
-//     private: false,
-//     component: Main,
-//   },
-// ]
-
-// const routes = [
-//   {
-//     path: paths.Importdata,
-//     exact: true,
-//     private: false,
-//     component: ImportData,
-//   },
-//   {
-//     path: paths.Profile,
-//     exact: true,
-//     private: false,
-//     component: Profile,
-//   },
-//   {
-//     path: paths.Payment,
-//     exact: true,
-//     private: false,
-//     component: Payment,
-//   },
-//   {
-//     path: paths.Upgrade,
-//     exact: true,
-//     private: false,
-//     component: Upgrade,
-//   },
-//   {
-//     path: paths.Desktop,
-//     exact: true,
-//     private: false,
-//     component: Desktop,
-//   },
-//   {
-//     path: paths.NewProfile,
-//     exact: true,
-//     private: false,
-//     component: NewProfile,
-//   },
-//   {
-//     path: paths.NewImportData,
-//     exact: true,
-//     private: false,
-//     component: NewImportData
-//   },
-//   {
-//     path: paths.DashBoard,
-//     exact: true,
-//     private: false,
-//     component: DashBoard
-//   },
-//   {
-//     private: false,
-//     component: NoMatchPage,
-//   },
-// ];
-
-
-function PublicRoute({ children, isAuthenticated, ...rest }) {
+function PublicRoute({ children, ...rest }) {
   return (
     <Route
       {...rest}
       render={
         ({ location }) => (
-          !isAuthenticated ? (
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: paths.Importdata,
-                state: { from: location }
-              }}
-            />
-          ))
+          !localStorage.getItem("token")
+            ? (
+              children
+            ) : (
+              <Redirect
+                to={{
+                  pathname: paths.Importdata,
+                  state: { from: location }
+                }}
+              />
+            ))
       }
     />
   );
 }
 
-function PrivateRoute({ children, isAuthenticated, ...rest }) {
+function PrivateRoute({ children, ...rest }) {
   return (
     <Route
       {...rest}
       render={
         ({ location }) => (
-          isAuthenticated
+          localStorage.getItem("token")
             ? (
               children
             ) : (
@@ -213,135 +76,114 @@ function PrivateRoute({ children, isAuthenticated, ...rest }) {
 }
 
 function App() {
-  const isAuthenticated = localStorage.getItem("token");
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-
-          <PublicRoute
-            path="/main"
-            isAuthenticated={isAuthenticated}
-          >
-            <Main />
-          </PublicRoute>
           <PublicRoute
             path={paths.Login}
-            isAuthenticated={isAuthenticated}
           >
             <LogIn />
           </PublicRoute>
           <PublicRoute
             path={paths.SignUp}
-            isAuthenticated={isAuthenticated}
           >
             <Registration />
           </PublicRoute>
           <PublicRoute
             path={paths.Verify}
-            isAuthenticated={isAuthenticated}
           >
             <VerifyEmail />
           </PublicRoute>
           <PublicRoute
             path={paths.OldVerification}
-            isAuthenticated={isAuthenticated}
           >
             <Verification />
           </PublicRoute>
           <PublicRoute
             path={paths.Email}
-            isAuthenticated={isAuthenticated}
           >
             <Email />
           </PublicRoute>
           <PublicRoute
             path={paths.ResetPassword}
-            isAuthenticated={isAuthenticated}
           >
             <ResetPassword />
           </PublicRoute>
           <PublicRoute
             path={paths.ContactUs}
-            isAuthenticated={isAuthenticated}
           >
             <ContactUs />
           </PublicRoute>
           <PublicRoute
             path={paths.Policy}
-            isAuthenticated={isAuthenticated}
           >
             <Policy />
           </PublicRoute>
           <PublicRoute
             path={paths.Terms}
-            isAuthenticated={isAuthenticated}
           >
             <Terms />
           </PublicRoute>
           {/*------------------------------------------------- new public pages---------------------------------------------------------------- */}
           <PublicRoute
             path={paths.NewRegistration}
-            isAuthenticated={isAuthenticated}
           >
             <NewRegistration />
           </PublicRoute>
           <PublicRoute
             path={paths.NewLogin}
-            isAuthenticated={isAuthenticated}
           >
             <NewLogin />
           </PublicRoute>
           <PublicRoute
             path={paths.NewEmail}
-            isAuthenticated={isAuthenticated}
           >
             <NewEmail />
           </PublicRoute>
           {/* ---------------------------------------------------privates------------------------------------------------------------------------- */}
           <PrivateRoute
-            path={paths.Importdata}
-            isAuthenticated={isAuthenticated}
-          >
-            <ImportData />
-          </PrivateRoute>
-          <PrivateRoute
             path={paths.Profile}
-            isAuthenticated={isAuthenticated}
           >
             <Profile />
           </PrivateRoute>
           <PrivateRoute
             path={paths.Upgrade}
-            isAuthenticated={isAuthenticated}
           >
             <Upgrade />
           </PrivateRoute>
           <PrivateRoute
             path={paths.Desktop}
-            isAuthenticated={isAuthenticated}
           >
             <Desktop />
+          </PrivateRoute>
+          <PrivateRoute
+            path={paths.Importdata}
+            exact={true}
+          >
+            <ImportData />
           </PrivateRoute>
           {/* --------------------------new private routes-------------------------------------------------------------------------------- */}
           <PrivateRoute
             path={paths.NewProfile}
-            isAuthenticated={isAuthenticated}
           >
             <NewProfile />
           </PrivateRoute>
           <PrivateRoute
             path={paths.NewImportData}
-            isAuthenticated={isAuthenticated}
           >
             <NewImportData />
           </PrivateRoute>
           <PrivateRoute
             path={paths.DashBoard}
-            isAuthenticated={isAuthenticated}
           >
             <DashBoard />
           </PrivateRoute>
+          <PublicRoute
+            path={paths.Main}
+          >
+            <Main />
+          </PublicRoute>
           <Route>
             <NoMatchPage />
           </Route>
