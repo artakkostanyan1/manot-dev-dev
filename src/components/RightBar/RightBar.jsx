c6cd0166aef265d4d2bf6c03e1f6411026c5ad96
Elen Avanesyan <eavanesyan@macadamian.com>
2021-12-14 15:15:19 +0400

    Annotation page redesign

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,82 +1,32 @@
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 import './RightBar.scss';
 
-const useStyles = makeStyles((theme) => ({
-    first_part: {
-        height: '7%',
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: '3px'
-    },
-    second_part: {
-        height: '7%',
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: '7px'
-    },
-    third_part: {
-        height: '7%',
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: '7px'
-    },
-    fourth_part: {
-        height: '13.5%',
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: '7px'
-    }
-}));
-
-
-function RightBar({ notes, detectOnSingleImage }) {
-    const classes = useStyles();
-
+function RightBar({notes, detectOnSingleImage}) {
     return (
-        <div className='right_bar_conatainer'>
+        <div className='right_bar_container'>
+            <div className='text_part'>
+                <b> Details about object </b>
+            </div>
 
-            <Paper elevation={4} className={classes.first_part}>
-                <div className='text_part'>
-                    Details about object
-                </div>
-            </Paper>
-
-            {console.log(`notes, rightbar`, notes)}
-            {Object.keys(notes).map((note, id) => notes[note] && (
-                <Paper key={id} elevation={4} className={classes.second_part}>
-                    <div className='paper_inner_div'>
-                        <div className='text_part'>{note}</div>
-                        <div className='counter_part'>{notes[note]}</div>
+            {(!Object.keys(notes).length) ? <div className='not_available'> Not available yet </div> :
+                Object.keys(notes).map((note, id) => notes[note] && (
+                    <div>
+                        <div className='paper_inner_div'>
+                            <div className='note_part'>{note}</div>
+                            <div className='counter_part'>{notes[note]}</div>
+                        </div>
+                        {(id !== Object.keys(notes).length - 1) && <hr/>}
                     </div>
-                </Paper>
-            ))}
-
-            {/* <Paper elevation={4} className={classes.third_part}>
-                <div className='paper_inner_div'>
-                    <div className='text_part'>Lorem ipsum</div>
-                    <div className='counter_part'>1</div>
-                </div>
-            </Paper>
-
-            <Paper elevation={4} className={classes.fourth_part}>
-                <div className='fourth_text_part'>
-                    has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essent
-                </div>
-            </Paper> */}
+                ))}
 
             <div className='btn_container'>
-
                 <div className='single_annotation_btn_container'>
                     <div className='single_annotation_btn_border'>
                         <button className='single_annotation_btn' onClick={detectOnSingleImage}>
-                            Detect on single image
+                            Detect object on current image
                         </button>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
